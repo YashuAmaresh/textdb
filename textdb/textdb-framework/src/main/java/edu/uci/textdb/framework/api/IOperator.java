658aaebecedd3130819c81273c66337788cd6da6
zuozhiw <zuozhiw@uci.edu>
2017-03-01 18:16:18 -0800

    finish IOperator's description:

--- textdb/textdb-framework/src/main/java/edu/uci/textdb/framework/api/IOperator.java
+++ textdb/textdb-framework/src/main/java/edu/uci/textdb/framework/api/IOperator.java
@@ -7,7 +7,21 @@ import edu.uci.ics.textdb.api.exception.TextDBException;
 /**
  * IOperator is the fundamental interface for all operator classes.
  * Each operator will implement:
- *   setInputOperator(IOperator... operators)
+ *   getInputArity:    describes how many input operators this operator needs
+ *   
+ *   setInputOperator: sets one or multiple operators as this operator's input operator(s)
+ *                       the number of operators it receives must be consistent with getInputArity()
+ *   
+ *   open:             opens the input operator(s), sets up the output schema, 
+ *                       and sets up resources if needed
+ *   
+ *   getNextTuple:     pulls the tuples from input operator(s) if needed, 
+ *                       and returns the next tuple this operator generates.
+ *   
+ *   close:            closes the input operator(s) and close resources if needed
+ *   
+ *   getOutputSchema:  returns the output schema of this operator
+ *   
  * 
  * @author Zuozhi Wang
  * Created on 03/01/2017
